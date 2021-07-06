3a23cb7b45b6b6a048d3839d6ae3e94edc048d1b
Freya Black <freyablackdev@gmail.com>
2021-07-06 16:03:33 -0400

    fixed styling/added footer

--- app.js
+++ app.js
@@ -54,79 +54,22 @@ let monhunriseidURL =  url + '/games/494393' + '?key=' + key
 // add to the end of url &page={number}
 
 
-async function fetchData(){
+async function fetchNES(){
+    let megaman2idURL =   url + '/games/53939' + '?key=' + key
+    let legendofzeldaidURL =  url + '/games/24072' + '?key=' + key
+    let mariobros3idURL =  url + '/games/24030' + '?key=' + key
+    let metroididURL =  url + '/games/24138' + '?key=' + key
+    let dragonQuest3idURL =  url + '/games/54821' + '?key=' + key
     const megaman2 = await fetch(megaman2idURL);
     const legendOfZelda = await fetch(legendofzeldaidURL);
     const marioBros3 = await fetch(mariobros3idURL);
     const metroid = await fetch(metroididURL);
     const dragonQuest3 = await fetch(dragonQuest3idURL);
-    const linkToPast = await fetch(linktopastidURL);
-    const superMetroid = await fetch(supermetroididURL);
-    const kirbySuperstar = await fetch(kirbysuperstaridURL);
-    const megamanx = await fetch(megamanxidURL);
-    const megaman7 = await fetch(megaman7idURL);
-    const ocarinaOfTime = await fetch(ocarinaoftimeidURL);
-    const majorasMask = await fetch(majorasmaskidURL);
-    const banjoKazooie = await fetch(banjokazooieURL);
-    const megaman64 = await fetch(megaman64URL);
-    const superMario64 = await fetch(supermario64idURL);
-    const lozTwilightPrincess = await fetch(loztwilightprincessidURL);
-    const ssbm = await fetch(ssbmidURL);
-    const lozWindWaker = await fetch(lozwindwakeridURL);
-    const superMarioSunshine = await fetch(supermariosunshineidURL);
-    const re4 = await fetch(re4idURL);
-    const superMarioGalaxy = await fetch(superMarioGalaxyidURL);
-    const superMarioGalaxy2 = await fetch(superMarioGalaxy2idURL);
-    const monhunTri = await fetch(monhuntriidURL);
-    const newSuperMarioBrosu = await fetch(newsupermariobrosuidURL);
-    const dkcTropicalFreeze = await fetch(dkctropicalfreezeidURL);
-    const zombieu = await fetch(zombieuidURL);
-    const ssbwiiu = await fetch(ssbwiiuidURL);
-    const bayonetta2 = await fetch(bayonetta2idURL);
-    const lozbotw = await fetch(lozbotwidURL);
-    const marioOdyssey = await fetch(marioOdysseyidURL);
-    const metroidDread = await fetch(metroiddreadidURL);
-    const fireEmblem3Houses = await fetch(fireemblem3housesidURL);
-    const monhunRise = await fetch(monhunriseidURL);
-
-
     const megaman2data = await megaman2.json();
     const legendOfZeldadata = await legendOfZelda.json();
     const marioBros3data= await marioBros3.json();
     const metroiddata = await metroid.json();
     const dragonQuest3data = await dragonQuest3.json();
-    const linkToPastdata = await linkToPast.json();
-    const superMetroiddata = await superMetroid.json();
-    const kirbySuperstardata = await kirbySuperstar.json();
-    const megamanxdata = await megamanx.json();
-    const megaman7data = await megaman7.json();
-    const ocarinaOfTimedata = await ocarinaOfTime.json();
-    const majorasMaskdata = await majorasMask.json();
-    const banjoKazooiedata = await banjoKazooie.json();
-    const megaman64data = await megaman64.json();
-    const superMario64data = await superMario64.json();
-    const lozTwilightPrincessdata = await lozTwilightPrincess.json();
-    const ssbmdata = await ssbm.json();
-    const lozWindWakerdata = await lozWindWaker.json();
-    const superMarioSunshinedata = await superMarioSunshine.json();
-    const re4data = await re4.json();
-    const superMarioGalaxydata = await superMarioGalaxy.json();
-    const superMarioGalaxy2data = await superMarioGalaxy2.json();
-    const monhunTridata = await monhunTri.json();
-    const newSuperMarioBrosudata = await newSuperMarioBrosu.json();
-    const dkcTropicalFreezedata = await dkcTropicalFreeze.json();
-    const zombieudata = await zombieu.json();
-    const ssbwiiudata = await ssbwiiu.json();
-    const bayonetta2data = await bayonetta2.json();
-    const lozbotwdata = await lozbotw.json();
-    const marioOdysseydata = await marioOdyssey.json();
-    const metroidDreaddata = await metroidDread.json();
-    const fireEmblem3housesdata = await fireEmblem3Houses.json();
-    const monhunRisedata = await monhunRise.json();
-    
-    
-
-    //! Info attatchment section
     //* Megaman 2 
     document.getElementById("mm2title").innerText = megaman2data.name;
     document.getElementById("mm2about").innerText = megaman2data.description_raw;
@@ -161,7 +104,24 @@ async function fetchData(){
     document.getElementById("DQ3img2").src = dragonQuest3data.background_image_additional;
     document.getElementById("DQ3title").innerText = dragonQuest3data.name;
     document.getElementById("DQ3dev").innerText = dragonQuest3data.developers[0].name;
+}
 
+async function fetchSNES(){
+    let linktopastidURL =  url + '/games/25096' + '?key=' + key
+    let supermetroididURL =  url + '/games/27022' + '?key=' + key
+    let kirbysuperstaridURL =  url + '/games/57399' + '?key=' + key
+    let megamanxidURL =  url + '/games/57422' + '?key=' + key
+    let megaman7idURL =  url + '/games/57420' + '?key=' + key
+    const linkToPast = await fetch(linktopastidURL);
+    const superMetroid = await fetch(supermetroididURL);
+    const kirbySuperstar = await fetch(kirbysuperstaridURL);
+    const megamanx = await fetch(megamanxidURL);
+    const megaman7 = await fetch(megaman7idURL);
+    const linkToPastdata = await linkToPast.json();
+    const superMetroiddata = await superMetroid.json();
+    const kirbySuperstardata = await kirbySuperstar.json();
+    const megamanxdata = await megamanx.json();
+    const megaman7data = await megaman7.json();
     //* LoZ Link to the Past
     document.getElementById("pasttitle").innerText = linkToPastdata.name;
     document.getElementById("pastabout").innerText = linkToPastdata.description_raw;
@@ -196,7 +156,24 @@ async function fetchData(){
     document.getElementById("MM7img2").src = megaman7data.background_image_additional;
     document.getElementById("MM7title").innerText = megaman7data.name;
     document.getElementById("MM7dev").innerText = megaman7data.developers[0].name;
+}
 
+async function fetchN64(){
+    let ocarinaoftimeidURL =  url + '/games/25097' + '?key=' + key
+    let majorasmaskidURL =  url + '/games/25924' + '?key=' + key
+    let banjokazooieURL =  url + '/games/28532' + '?key=' + key
+    let megaman64URL =  url + '/games/390532' + '?key=' + key
+    let supermario64idURL =  url + '/games/54528' + '?key=' + key
+    const ocarinaOfTime = await fetch(ocarinaoftimeidURL);
+    const majorasMask = await fetch(majorasmaskidURL);
+    const banjoKazooie = await fetch(banjokazooieURL);
+    const megaman64 = await fetch(megaman64URL);
+    const superMario64 = await fetch(supermario64idURL);
+    const ocarinaOfTimedata = await ocarinaOfTime.json();
+    const majorasMaskdata = await majorasMask.json();
+    const banjoKazooiedata = await banjoKazooie.json();
+    const megaman64data = await megaman64.json();
+    const superMario64data = await superMario64.json();
     //* LoZ Ocarina of Time
     document.getElementById("OOTabout").innerText = ocarinaOfTimedata.description_raw
     document.getElementById("OOTimg1").src = ocarinaOfTimedata.background_image
@@ -231,7 +208,24 @@ async function fetchData(){
     document.getElementById("Mario64img2").src = superMario64data.background_image_additional
     document.getElementById("Mario64title").innerText = superMario64data.name
     document.getElementById("Mario64dev").innerText = superMario64data.developers[0].name
+}
 
+async function fetchGCN(){
+    let loztwilightprincessidURL =  url + '/games/27015' + '?key=' + key
+    let ssbmidURL =  url + '/games/56222' + '?key=' + key
+    let lozwindwakeridURL =  url + '/games/56092' + '?key=' + key
+    let supermariosunshineidURL =  url + '/games/52371' + '?key=' + key
+    let re4idURL =  url + '/games/56184' + '?key=' + key
+    const lozTwilightPrincess = await fetch(loztwilightprincessidURL);
+    const ssbm = await fetch(ssbmidURL);
+    const lozWindWaker = await fetch(lozwindwakeridURL);
+    const superMarioSunshine = await fetch(supermariosunshineidURL);
+    const re4 = await fetch(re4idURL);
+    const lozTwilightPrincessdata = await lozTwilightPrincess.json();
+    const ssbmdata = await ssbm.json();
+    const lozWindWakerdata = await lozWindWaker.json();
+    const superMarioSunshinedata = await superMarioSunshine.json();
+    const re4data = await re4.json();
     //* LoZ Twilight Princess
     document.getElementById("TwiPrinabout").innerText = lozTwilightPrincessdata.description_raw
     document.getElementById("TwiPrinimg1").src = lozTwilightPrincessdata.background_image
@@ -266,7 +260,18 @@ async function fetchData(){
     document.getElementById("RE4img2").src = re4data.background_image_additional
     document.getElementById("RE4title").innerText = re4data.name
     document.getElementById("RE4dev").innerText = re4data.developers[0].name
+}
 
+async function fetchWii(){
+    let superMarioGalaxyidURL =  url + '/games/27024' + '?key=' + key
+    let superMarioGalaxy2idURL =  url + '/games/27036' + '?key=' + key
+    let monhuntriidURL =  url + '/games/24411' + '?key=' + key
+    const superMarioGalaxy = await fetch(superMarioGalaxyidURL);
+    const superMarioGalaxy2 = await fetch(superMarioGalaxy2idURL);
+    const monhunTri = await fetch(monhuntriidURL);
+    const superMarioGalaxydata = await superMarioGalaxy.json();
+    const superMarioGalaxy2data = await superMarioGalaxy2.json();
+    const monhunTridata = await monhunTri.json();
     //* Super Mario Galaxy
     document.getElementById("Galaxyabout").innerText = superMarioGalaxydata.description_raw
     document.getElementById("Galaxyimg1").src = superMarioGalaxydata.background_image
@@ -287,7 +292,24 @@ async function fetchData(){
     document.getElementById("MHTimg2").src = monhunTridata.background_image_additional
     document.getElementById("MHTtitle").innerText = monhunTridata.name
     document.getElementById("MHTdev").innerText = monhunTridata.developers[0].name
+}
 
+async function fetchWiiU(){
+    let newsupermariobrosuidURL =  url + '/games/27967' + '?key=' + key
+    let dkctropicalfreezeidURL =  url + '/games/27978' + '?key=' + key
+    let zombieuidURL =  url + '/games/27192' + '?key=' + key
+    let ssbwiiuidURL =  url + '/games/331575' + '?key=' + key
+    let bayonetta2idURL =  url + '/games/23760' + '?key=' + key
+    const newSuperMarioBrosu = await fetch(newsupermariobrosuidURL);
+    const dkcTropicalFreeze = await fetch(dkctropicalfreezeidURL);
+    const zombieu = await fetch(zombieuidURL);
+    const ssbwiiu = await fetch(ssbwiiuidURL);
+    const bayonetta2 = await fetch(bayonetta2idURL);
+    const newSuperMarioBrosudata = await newSuperMarioBrosu.json();
+    const dkcTropicalFreezedata = await dkcTropicalFreeze.json();
+    const zombieudata = await zombieu.json();
+    const ssbwiiudata = await ssbwiiu.json();
+    const bayonetta2data = await bayonetta2.json();
     //* New Super Mario Bros U
     document.getElementById("MBUabout").innerText = newSuperMarioBrosudata.description_raw
     document.getElementById("MBUimg1").src = newSuperMarioBrosudata.background_image
@@ -322,7 +344,24 @@ async function fetchData(){
     document.getElementById("Bayoimg2").src = bayonetta2data.background_image_additional
     document.getElementById("Bayotitle").innerText = bayonetta2data.name
     document.getElementById("Bayodev").innerText = bayonetta2data.developers[0].name
+}
 
+async function fetchSwitch(){
+    let lozbotwidURL =  url + '/games/22511' + '?key=' + key
+    let marioOdysseyidURL =  url + '/games/28026' + '?key=' + key
+    let metroiddreadidURL =  url + '/games/622495' + '?key=' + key
+    let fireemblem3housesidURL =  url + '/games/246478' + '?key=' + key
+    let monhunriseidURL =  url + '/games/494393' + '?key=' + key
+    const lozbotw = await fetch(lozbotwidURL);
+    const marioOdyssey = await fetch(marioOdysseyidURL);
+    const metroidDread = await fetch(metroiddreadidURL);
+    const fireEmblem3Houses = await fetch(fireemblem3housesidURL);
+    const monhunRise = await fetch(monhunriseidURL);
+    const lozbotwdata = await lozbotw.json();
+    const marioOdysseydata = await marioOdyssey.json();
+    const metroidDreaddata = await metroidDread.json();
+    const fireEmblem3housesdata = await fireEmblem3Houses.json();
+    const monhunRisedata = await monhunRise.json();
     //* LoZ Breath of the Wild
     document.getElementById("BotWabout").innerText = lozbotwdata.description_raw
     document.getElementById("BotWimg1").src = lozbotwdata.background_image
@@ -345,11 +384,11 @@ async function fetchData(){
     document.getElementById("Dreaddev").innerText = metroidDreaddata.developers[0].name
 
     //* Fire Emblem 3 Houses
-    document.getElementById("3Houseabout").innerText = fireEmblem3housesdata.description_raw
-    document.getElementById("3Houseimg1").src = fireEmblem3housesdata.background_image
-    document.getElementById("3Houseimg2").src = fireEmblem3housesdata.background_image_additional
-    document.getElementById("3Housetitle").innerText = fireEmblem3housesdata.name
-    document.getElementById("3Housedev").innerText = fireEmblem3housesdata.developers[0].name
+    document.getElementById("Emblemabout").innerText = fireEmblem3housesdata.description
+    document.getElementById("Emblemimg1").src = fireEmblem3housesdata.background_image
+    document.getElementById("Emblemimg2").src = fireEmblem3housesdata.background_image_additional
+    document.getElementById("Emblemtitle").innerText = fireEmblem3housesdata.name
+    document.getElementById("Emblemdev").innerText = fireEmblem3housesdata.developers[0].name
 
     //* Monster Hunter Rise
     document.getElementById("MHRiseabout").innerText = monhunRisedata.description_raw
@@ -357,7 +396,13 @@ async function fetchData(){
     document.getElementById("MHRiseimg2").src = monhunRisedata.background_image_additional
     document.getElementById("MHRisetitle").innerText = monhunRisedata.name
     document.getElementById("MHRisedev").innerText = monhunRisedata.developers[0].name
-
+    console.log(fireEmblem3housesdata)
 }
 
-fetchData()
+fetchNES()
+fetchSNES()
+fetchN64()
+fetchGCN()
+fetchWii()
+fetchWiiU()
+fetchSwitch()
